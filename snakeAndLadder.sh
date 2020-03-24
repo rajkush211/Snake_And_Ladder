82ba523f491c5b26ea6e5260a6099b4337211f28
rajkush211 <rajkush211.rk@gmail.com>
2020-03-25 04:03:30 +0530

    ADDED: Player checks for an option

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -9,11 +9,32 @@ clear
 #CONSTANTS
 START_POSITION=0
 
+#VARIABLES
+newPosition=0
+
 function rollDie() {
 	result=$(( RANDOM % 6 + 1 ))
 	echo $result
 }
 
-rollDie
+function option() {
+	choice=$(( RANDOM % 3 + 1 ))
+	echo $choice
+}
+
+function playerOption() {
+	operation=$( option )
+	case $operation in
+		1)
+			newPosition=0;;
+		2)
+			newPosition=$(( $newPosition + $( rollDie ) ));;
+		3)
+			newPosition=$(( $newPosition - $( rollDie ) ));;
+	esac
+	echo $newPosition
+}
+
+playerOption
 
 sleep 1
