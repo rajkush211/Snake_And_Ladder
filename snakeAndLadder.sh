d0307b3ec88edb19b3421407bb851f996d153301
rajkush211 <rajkush211.rk@gmail.com>
2020-03-25 07:02:22 +0530

    ADDED: Played with two players at the same time

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -11,9 +11,13 @@ START_POSITION=0
 WINNING_POSITION=100
 
 #VARIABLES
-newPosition=0
-prevPosition=0
+newPosition1=0
+prevPosition1=0
+newPosition2=0
+prevPosition2=0
 diceRolledCount=0
+playerTurn=0
+
 
 function rollDie() {
 	result=$(( RANDOM % 6 + 1 ))
@@ -26,44 +30,80 @@ function option() {
 }
 
 function playerOption() {
-	while [[ $newPosition -lt $WINNING_POSITION ]]
+	playerTurn=1
+	operation=0
+	while [[ $newPosition1 -lt $WINNING_POSITION && $newPosition2 -lt $WINNING_POSITION ]]
 	do
+		echo -e "\nPlaying $playerTurn player"
 		operation=$( option )
+		if [ $operation -ne 1 ]
+		then
+			(( diceRolledCount++ ))
+		fi
 		case $operation in
 			1)
-				prevPosition=$newPosition
-				newPosition=$newPosition;;
+				echo "NO PLAY"
+				if [ $playerTurn -eq 1 ]
+				then
+					prevPosition1=$newPosition1
+					newPosition1=$newPosition1
+					echo "Position of player 1 is still $newPosition1"
+				else
+					prevPosition2=$newPosition2
+					newPosition2=$newPosition2
+					echo "Position of player 2 is still $newPosition2"
+				fi;;
 			2)
-				prevPosition=$newPosition
-				newPosition=$(( $newPosition + $( rollDie ) ))
-				(( diceRolledCount++ )) 
-				if [[ $newPosition -gt $WINNING_POSITION ]]
+				echo "LADDER"
+				if [ $playerTurn -eq 1 ]
 				then
-					newPosition=$prevPosition
+					prevPosition1=$newPosition1
+					newPosition1=$(( $newPosition1 + $( rollDie ) ))
+					if [[ $newPosition1 -gt $WINNING_POSITION ]]
+					then
+						newPosition1=$prevPosition1
+					fi
+					echo "Position of player 1 after Dice rolled $newPosition1"
+				else
+					prevPosition2=$newPosition2
+					newPosition2=$(( $newPosition2 + $( rollDie ) ))
+					if [[ $newPosition2 -gt $WINNING_POSITION ]]
+					then
+						newPosition2=$prevPosition2
+					fi
+					echo "Position of player 2 after Dice rolled $newPosition2"
 				fi;;
-#				echo "Position after Dice rolled $newPosition";;
 			3)
-				prevPosition=$newPosition
-				newPosition=$(( $newPosition - $( rollDie ) ))
-				(( diceRolledCount++ ))
-				if [[ $newPosition -lt  0 ]]
+				echo "SNAKE"
+				if [ $playerTurn -eq 1 ]
 				then
-					newPosition=0
+					prevPosition1=$newPosition1
+					newPosition1=$(( $newPosition1 - $( rollDie ) ))
+					if [[ $newPosition1 -lt  0 ]]
+					then
+						newPosition1=0
+					fi
+					echo "Position of player1 after Dice rolled $newPosition1"
+				else
+					prevPosition2=$newPosition2
+					newPosition2=$(( $newPosition2 - $( rollDie ) ))
+					if [[ $newPosition2 -lt  0 ]]
+					then
+						newPosition2=0
+					fi
+						echo "Position of player2 after Dice rolled $newPosition2"
 				fi;;
-#				echo "Position after Dice rolled $newPosition";;
 		esac
+		playerTurn=$(( 3 - playerTurn ))
 	done
-	echo "$diceRolledCount"
+	echo -e "\nThe Dice rolled $diceRolledCount times"
 }
 
 function snakeAndLadderGame() {
-	read -p "Enter Player1 name: " player1
-	read -p "Enter Player2 name: " player2
-	player1Game=$( playerOption )
-	player2Game=$( playerOption )
-	echo -e "\n$player1 rolled the Dice for $player1Game times"
-	echo "$player2 rolled the Dice for $player2Game times"
-	if [[ $player1Game -lt $player2Game ]]
+	read -p "Enter Player 1 name: " player1
+	read -p "Enter Player 2 name: " player2
+	playerOption
+	if [[ $newPosition1 -eq 100 ]]
 	then
 		echo -e "\nCongratulations $player1 you won"
 	else
